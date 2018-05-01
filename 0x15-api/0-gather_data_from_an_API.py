9a286c0632d6b76002864b67c7a522509879751a
JacobBHartman <209@holbertonschool.com>
2018-04-30 17:54:50 -0700

    change tasks_titles to list from string

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,14 +21,15 @@ if __name__ == '__main__':
     url_employee_todos = "{}/todos?userId={}".format(url_api, employee_id)
     employee_todos = requests.get(url_employee_todos).json()
 
-    tasks_titles = ""
+    tasks_titles = []
     task_count = 0
     for tasks in employee_todos:
         if tasks['completed'] is True:
-            tasks_titles += '\t{}\n'.format(tasks['title'])
+            tasks_titles.append(tasks['title'])
             task_count += 1
 
     print("Employee {} is done with tasks({}/{})".format(employee_name,
                                                          task_count,
                                                          len(employee_todos)))
-    print(tasks_titles, end="")
+    for task in tasks_titles:
+        print("\t {}".format(task))
